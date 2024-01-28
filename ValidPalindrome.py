aad132ce32f370d12e35c105cbc18454e6434b07
tahervora <tahervora@Tahers-Air.attlocal.net>
2024-01-28 16:36:55 -0500

    Day#7

---
+++ ValidPalindrome.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/valid-palindrome/description/
+
+s = "A man, a plan, a canal: Panama"
+
+def validPalindrome(s):
+    s=s.lower()
+    l,r= 0,len(s)-1
+
+    while l<r:
+        if not s[l].isalnum():
+            l+=1
+        elif not s[r].isalnum():
+            r-=1
+        elif s[l]!=s[r]:
+            return False
+        else:
+            l+=1
+            r-=1
+    return True
+
+print(validPalindrome(s))
